96d3a3a13c2c3c9ae651fdfe8069ebc418ed6c12
lucas <lucas.mslima@perpart.pe.gov.br>
2025-08-08 16:36:11 -0300

    Add resizable panels to dashboard layout and create SectionDashboard component

---
+++ src/components/myComponents/sectionDashboard.tsx
@@ -0,0 +1,8 @@
+export default function SectionDashboard() {
+    return (
+        <div className="flex flex-col items-center justify-center rounded-md !h-185 bg-white">
+            <h1 className="text-2xl text-black mb-4">Dashboard Section</h1>
+            <p className="text-black">This is the dashboard section content.</p>
+        </div>
+    )
+}
