a0a635f181d74c4a7ae0d8f68c11e2033f7fe36c
lucas <lucas.mslima@perpart.pe.gov.br>
2025-09-01 15:08:28 -0300

    feat: update task completion status in fakeApiLocal.json; modify ItemDashboard to use correct tab values and enhance Task interface with optional timeCompleted property

--- src/components/myComponents/itemDashboard/itemDashboard.tsx
+++ src/components/myComponents/itemDashboard/itemDashboard.tsx
@@ -7,12 +7,12 @@ export default function ItemDashboard() {
         <Tabs defaultValue="todolist" className="w-[100%] h-[100%] pt-1 rounded-lg bg-stone-200">
             <TabsList className="gap-2 flex justify-center w-[100%] bg-stone-300 rounded-t-lg  md:w-[30%] md:ml-[3.5vw]">
                 <TabsTrigger value="todolist" className="text-white !bg-teal-950">To do list</TabsTrigger>
-                <TabsTrigger value="Carousel" className="text-white !bg-teal-950">Carousel</TabsTrigger>
+                <TabsTrigger value="Chart" className="text-white !bg-teal-950">Chart</TabsTrigger>
             </TabsList>
             <TabsContent value="todolist" className="w-[100%] !h-[100%]">
                 <ToList />
             </TabsContent>
-            <TabsContent value="Carousel" className="flex flex-column align-center justify-center align-end h-[100%] bg-stone-500">
+            <TabsContent value="Chart" className="flex flex-column align-center justify-center align-end h-[100%]">
                 <ChartDashboard />
             </TabsContent>
         </Tabs>
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export interface Task {
   id: string;
   text: string;
   time: string;
-  date: string
+  timeCompleted?: string;
+  date: string;
   completed: boolean;
 }
