213f6dad4a5ef083fcb0a7174b6536c24943b6f9
lucas <lucas.mslima@perpart.pe.gov.br>
2025-09-02 14:36:57 -0300

    feat: rename item

--- src/components/myComponents/itemDashboard/itemTabDash.tsx
+++ src/components/myComponents/itemDashboard/itemTabDash.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ToList from "./itens/toDoList"
 import ChartDashboard from "./itens/chart"
 
-export default function ItemDashboard() {
+export default function ItemTabDash() {
     return (
         <Tabs defaultValue="todolist" className="w-[100%] h-[100%] pt-1 rounded-lg bg-stone-200">
             <TabsList className="gap-2 flex justify-center w-[100%] bg-stone-300 rounded-t-lg  md:w-[30%] md:ml-[3.5vw]">
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,11 @@
-import ItemDashboard from "@/components/myComponents/itemDashboard/itemDashboard";
+import ItemTabDash from "@/components/myComponents/itemDashboard/itemTabDash";
 
 export default function Dashboard() {
     return (
         <main className="flex flex-row items-center justify-center h-[100vh] w-[100vw] p-0">
             {/* Painel do conteúdo */}
             <div className="flex justify-center !w-[95%] h-[100%] rounded-lg p-5">
-                <ItemDashboard />
+                <ItemTabDash />
             </div>
         </main>
     )
