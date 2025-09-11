6f9a893b756ec083b11407154d71b6d1dad0d0a8
lucas <lucas.mslima@perpart.pe.gov.br>
2025-09-11 10:41:33 -0300

    feat: add styling to ChartDashboard component for better alignment

--- src/components/myComponents/itemDashboard/itemTabDash.tsx
+++ src/components/myComponents/itemDashboard/itemTabDash.tsx
@@ -18,7 +18,9 @@ export default function ItemTabDash() {
                         <h2 className="font-bold mb-8">Chart</h2>
                         <h4 className="font-bold mb-4">Tasks completed and incomplete</h4>
                     </div>
-                    <ChartDashboard />
+                    <div className="flex justify-center align-center">
+                        <ChartDashboard />
+                    </div>
                     <div className="px-4">
                         <h6 className="font-bold mb-4">About</h6>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
